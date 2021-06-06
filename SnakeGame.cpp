e8c8dc4b41b854fd55ba519bd756d218be28decf
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-06 12:09:14 +0200

    Started to creating snake game with c++

---
+++ SnakeGame.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+bool gameOver;
+
+void Setup() {
+
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
