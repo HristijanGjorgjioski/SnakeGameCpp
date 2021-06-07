9c695f0c5e330004f71c8b18f75f4a24c8bb8b3e
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-07 07:45:49 +0200

    Working on Snake Tails logic

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -6,6 +6,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -74,6 +76,17 @@ void Input() {
 }
 
 void Logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	for (int i = 1; i < nTail; i++) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
 	switch (dir) {
 		case LEFT:
 			x--;
@@ -98,6 +111,7 @@ void Logic() {
 		score += 10;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
+		nTail++;
 	}
 }
 
